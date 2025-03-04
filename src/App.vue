cee43c8b4a94ec62ae0a98bc79f44c0d95619b2e
job moore <jobmurdan@ohtmail.com>
2025-03-04 14:25:59 -0700

    agregar lista de tareas usando v-model para entrada y v-for para renderizado

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue';
 import Contador from './components/Contador.vue';
+import ListaDeTareas from './components/ListaDeTareas.vue';
 </script>
 
 <template>
 <Contador></Contador>
+<ListaDeTareas></ListaDeTareas>
 </template>
 
 <style scoped>
---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const addTarea = () =>{tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value='';
+}
+</script>
+
+
+
+<template>
+    <h2>Lista De Tareas</h2>
+    <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+    <ul>
+        <li v-for="(tarea,index) in tareas" :key="index">
+            {{ tarea }}
+        </li>
+        <!-- <li>tarea</li> -->
+    </ul>
+</template>
+
+
+
+
+
+<style scoped>
+
+</style>
