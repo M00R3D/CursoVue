71c7df9c05736ea61df7c74fa4c59bf9e9b926ea
job moore <jobmurdan@ohtmail.com>
2025-03-10 20:25:28 -0700

    uso de v show v if v else y v else if

--- src/modules/calcular/views/CalcularView.vue
+++ src/modules/calcular/views/CalcularView.vue
@@ -5,13 +5,18 @@ const valor = ref('');
 const valorCalculado = computed(()=>{
     return valor.value *3;
 });
+const mostrarElemento = computed(()=>{
+    return valor.value > 10;    
+});
 </script>
 
 <template>
     <h3>Propiedades Computadas</h3>
     <input type="text" v-model="valor">
     {{ valorCalculado }}
-    <ResultadoSection resultado="valor calculado"></ResultadoSection>
+    <ResultadoSection resultado="valor calculado" v-if="mostrarElemento"></ResultadoSection>
+    <div v-else-if="isNaN(valor)">debe ser un numero</div>
+    <div v-else>contenido de v-else</div>
 </template>
 
 <style scoped>
